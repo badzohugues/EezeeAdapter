b2c8e420173fb724b38e47b416a697869c66e327
badzohugues <hugues.badzoukoula@gmail.com>
2021-03-30 00:18:56 +0200

    Cleaning code, rename and update packages

--- Sample/app/src/main/java/com/badzohugues/sample/ui/home/HomeActivity.kt
+++ Sample/app/src/main/java/com/badzohugues/sample/ui/home/HomeActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.badzohugues.eezeeadapter.EezeeAdapter
+import com.badzohugues.splitzadapter.SplitzAdapter
 import com.badzohugues.sample.R
 import com.badzohugues.sample.databinding.ActivityHomeBinding
+import com.badzohugues.sample.misc.SpacingDecoration
 import com.badzohugues.sample.model.Person
 import com.badzohugues.sample.ui.home.presenter.HomePresenter
 
@@ -18,7 +18,7 @@ class HomeActivity : AppCompatActivity(), View {
 
     private lateinit var binding: ActivityHomeBinding
     private val presenter by lazy { HomePresenter(this) }
-    private val eezeeAdapter by lazy { EezeeAdapter<Person>() }
+    private val adapter by lazy { SplitzAdapter<Person>() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +35,9 @@ class HomeActivity : AppCompatActivity(), View {
     @SuppressLint("SetTextI18n")
     private fun initRecyclerView() {
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        val decoration = DividerItemDecoration(this, layoutManager.orientation)
+        val decoration = SpacingDecoration(8)
 
-        eezeeAdapter.layout(R.layout.item_recyclerview)
+        adapter.layout(R.layout.item_recyclerview)
             .items(ArrayList())
             .onBind { item ->
                 val firstNameTxv: TextView = findViewById(R.id.item_firstname_txv)
@@ -54,11 +54,11 @@ class HomeActivity : AppCompatActivity(), View {
         with(binding) {
             homeRecyclerview.layoutManager = layoutManager
             homeRecyclerview.addItemDecoration(decoration)
-            homeRecyclerview.adapter = eezeeAdapter
+            homeRecyclerview.adapter = adapter
         }
     }
 
     override fun fillPersons(persons: List<Person>) {
-        eezeeAdapter.items = persons
+        adapter.items = persons
     }
 }
